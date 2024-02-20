059b2dfe62c6197cd0d09a7023ec62c91979d818
croissain <nhattho03@gmail.com>
2024-02-20 11:12:05 +0700

    format number

--- src/utils/formatBigNumber.ts
+++ src/utils/formatBigNumber.ts
@@ -1,5 +1,18 @@
 export const formatBigNumber = (value: number, toFixed: number = 9): string => {
-  return (Number(value) / Number(10n ** 18n))
+  let parts = (Number(value) / Number(10n ** 18n))
     .toFixed(toFixed)
     .replace(/\.?0+$/, "");
+
+  const [integerPart, decimalPart] = parts.split(".");
+
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  const formattedDecimal = decimalPart
+    ? decimalPart.replace(/(\d{3})/g, "$1 ")
+    : "";
+
+  const formattedValue = formattedDecimal
+    ? `${formattedInteger}.${formattedDecimal.trim()}`
+    : formattedInteger;
+
+  return formattedValue;
 };
